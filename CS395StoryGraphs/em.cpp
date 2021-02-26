a06b883e407a4963918332f2f1b0a4b0fc0ccf4b
thezacfoster <thezacfoster@gmail.com>
2021-02-26 04:50:08 -0500

    Initial setup. Created main execution file and started recycling some of the most common functions from previous Camelot project

---
+++ CS395StoryGraphs/em.cpp
@@ -0,0 +1,7 @@
+// File Description: The main execution file for this experience manager.
+#pragma once
+
+int main() {
+
+	return 0;
+}
---
+++ CS395StoryGraphs/functions.h
@@ -0,0 +1,18 @@
+// File Description: A handful of functions that simplify the execution of common processes in Camelot.
+
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Pauses execution in the experience manager until the given input message is received from Camelot
+void Wait(string msg);
+
+// Sends a given command to Camelot. If waitForSuccess is true, pauses the experience manager's execution until the command succeeds.
+void Action(string cmd, bool waitForSuccess);
+
+// Manages the entire process of setting up a character.
+void SetUpCharacter(string name, string bodyType, string clothing, string hairStyle, string hairColor, string position)
+
+// Manages the entire process of setting up a
